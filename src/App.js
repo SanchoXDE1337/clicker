7ad9be33340257db5d438991674a2a922709b6f9
Алешенька:3 <alexnagorny.an@gmail.com>
2019-09-13 00:23:10 +0300

    drink and code :beers:

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const MAX_VALUE = 10;
+const MIN_VALUE = 0;
+
+class App extends React.Component {
+
+    state = {value: 0};
+
+    handleDecrement = () => this.state.value !== MIN_VALUE && this.setState({value: this.state.value - 1});
+
+    handleIncrement = () => this.state.value !== MAX_VALUE && this.setState({value: this.state.value + 1});
+
+    handleChange = (e) => (e.target.value <= MAX_VALUE && e.target.value >= MIN_VALUE)
+        && this.setState({value: +e.target.value});
+
+    inputClick = (e) => e.target.select();
+
+    render() {
+        const {value} = this.state;
+        return <div>
+            <button onClick={this.handleDecrement}>-</button>
+            <input type="number" value={value} onChange={this.handleChange} onClick={this.inputClick}/>
+            <button onClick={this.handleIncrement}>+</button>
+        </div>
+
+    }
 }
 
 export default App;
